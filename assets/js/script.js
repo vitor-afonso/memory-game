ccf7e48c047a0b5b19b4a551e27de622125a441d
Vittokm <vittokm@gmail.com>
2021-03-19 00:18:06 +0100

    comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ let picID2;
 function show(x) {
 
     
-    let allDiv = document.querySelector("#group1");
-    let allPic = document.getElementsByTagName("img");
+    /*let allDiv = document.querySelector("#group1");
+    let allPic = document.getElementsByTagName("img");*/
     let picArea = x;
     let pic = x.querySelector("img");
     let attempts = document.querySelector("#attemptNum");
@@ -30,8 +30,8 @@ function show(x) {
 
     if (num === 2 && picID != picID2) {
         
-        allDiv.querySelectorAll(".g-area").style.backgroundColor = "#00F";
-        allPic.style.cssText = "visibility: hidden;";
+        /*allDiv.querySelectorAll(".g-area").style.backgroundColor = "#00F";
+        allPic.style.cssText = "visibility: hidden;";*/
         num = 0;  
 
     }
