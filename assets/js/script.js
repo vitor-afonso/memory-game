ebab6962d2a9708247cb9bca58441109cc498765
Vitor Afonso <vittokm@hotmail.com>
2021-09-06 21:40:17 +0200

    added settimeout to the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 /* jshint esversion: 6*/
+
 window.onload = function() {
     
     let images = document.querySelectorAll("#group1 > div");
@@ -8,32 +9,61 @@ window.onload = function() {
 
 };
 
+let card1;
+let card2;
+let card1Src;
+let card2Src;
+let cardCount = 0;
+
+
 function show(objEvent) {
     
     let cardArea = objEvent.target;
     let card = cardArea.querySelector("img");
-    //let cardNumbers = 1;
+
 
     if (card.classList.contains("hidded")) {
-
         
         card.classList.remove("hidded");
         card.classList.add("makeVisible"); 
-        cardNumbers++;
-    }
-    /*
-    if (cardNumbers === 2) {
+        
 
-        setTimeout(makeHidden, 3000, card);
+        if (cardCount === 0) {
 
+            card1Src = card.src;
+            card1 = card;
+            cardCount++;
+            return;
+        }
+
+        if (cardCount === 1) {
+
+            card2Src = card.src;
+            card2 = card;
+
+            if (card1Src === card2Src) {
+
+                console.log("Funcionou!");
+                
+            } else {
+                setTimeout(makeHidden, 2000, card1, card2);
+                
+            }
+            cardCount = 0;
+        }
     }
-    */
     
 }
-function makeHidden(card) {
 
-    card.classList.remove("makeVisible");
-    card.classList.add("hidded");
+
+function makeHidden(card1, card2) {
+
+    card1.classList.remove("makeVisible");
+    card1.classList.add("hidded");
+
+    card2.classList.remove("makeVisible");
+    card2.classList.add("hidded");
 
 }
-// Think about another function to compare the choices 
+
+// disable clicking 2x in the same image
