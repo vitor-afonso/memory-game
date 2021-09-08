6d9bab46ff9c20ad6874a65ac6e252bc8f34a4ba
Vitor Afonso <vittokm@hotmail.com>
2021-09-08 20:53:56 +0200

    disabled the cklicking if image is visible

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,57 +15,85 @@ let card1Src;
 let card2Src;
 let cardCount = 0;
 
-
 function show(objEvent) {
     
     let cardArea = objEvent.target;
     let card = cardArea.querySelector("img");
 
+    if (cardCount > 0 && card === null) { 
 
-    if (card.classList.contains("hidded")) {
-        
-        card.classList.remove("hidded");
-        card.classList.add("makeVisible"); 
-        
+        return;
+    } 
 
-        if (cardCount === 0) {
+    card.classList.remove("hidded");
+    card.classList.add("makeVisible");
 
-            card1Src = card.src;
-            card1 = card;
-            cardCount++;
-            return;
-        }
+    if (cardCount === 0) {
 
-        if (cardCount === 1) {
-
-            card2Src = card.src;
-            card2 = card;
-
-            if (card1Src === card2Src) {
-
-                document.querySelector("#scoreNum").innerHTML++;
-
-        
-                if (document.querySelector("#scoreNum").innerHTML == 3) {
-
-                    let message = document.querySelector("#winningResult");
-                    let totalScore =  document.querySelector("#scoreNum").innerHTML;
-                    let totalFailedAttempts = document.querySelector("#attemptNum").innerHTML;
-                    let totalResult = parseInt(totalScore) + parseInt(totalFailedAttempts);
-                    
-                    message.innerHTML = `Congratulations!! You won the game in ${totalResult} attempts.`;
-                }
-                
-            } else {
-
-                setTimeout(makeHidden, 2000, card1, card2);
-                document.querySelector("#attemptNum").innerHTML++;
-                
-            }
-            cardCount = 0;
-        }
+        card1 = card;
+        card1Src = card.src;
+        cardCount++;
+        setTimeout(hideCard1, 5000, card1);
+        return;
     }
-    
+
+    if (cardCount === 1) {
+
+        card2 = card;
+        card2Src = card.src;
+        
+
+        if (card1Src === card2Src) {
+
+            card1.classList.add("done");
+            card2.classList.add("done");
+            
+            document.querySelector("#scoreNum").innerHTML++;
+
+        
+            if (document.querySelector("#scoreNum").innerHTML == 3) {
+
+                let message = document.querySelector("#winningResult");
+                let totalScore =  document.querySelector("#scoreNum").innerHTML;
+                let totalFailedAttempts = document.querySelector("#attemptNum").innerHTML;
+                let totalResult = parseInt(totalScore) + parseInt(totalFailedAttempts);
+                let images = document.querySelectorAll("#group1 > div");
+                    
+                message.innerHTML = `Congratulations!! You won the game in ${totalResult} attempts.`;
+                
+                for (let i = 0; i < images.length; i++) {
+                    images[i].onclick = " ";
+                }
+            }
+                
+        } else {
+            
+            // Removes the onclick show function
+            let images = document.querySelectorAll("#group1 > div");
+            for (let i = 0; i < images.length; i++) {
+                images[i].onclick = " ";
+            }
+            
+            
+            setTimeout(makeHidden, 2000, card1, card2);
+            document.querySelector("#attemptNum").innerHTML++;
+            
+        }
+        cardCount = 0;
+    }
+    return;
+}
+
+//Turns the first card back if no other selection is made within 5 sec
+function hideCard1(card1) {
+
+    if (card1.classList.contains("makeVisible") && !card1.classList.contains("done")) {
+
+        card1.classList.remove("makeVisible");
+        card1.classList.add("hidded");
+        card1Src = "";
+        cardCount = 0;
+    }
 }
 
 
@@ -77,6 +105,12 @@ function makeHidden(card1, card2) {
     card2.classList.remove("makeVisible");
     card2.classList.add("hidded");
 
+    // Reactivates the onclick show function
+    let images = document.querySelectorAll("#group1 > div");            
+    for (let i = 0; i < images.length; i++) {
+        images[i].onclick = show;
+    }
+
 }
 
-// disable clicking 2x in the same image
+// make it random the images every time
